f3f159764ac2d5daa3a40f913fd954ccd19fd663
ashun <you@example.com>
2019-05-20 10:12:23 +0800

    1

--- static/static/footer.js
+++
@@ -1,7 +0,0 @@
-function killerrors() {
-	return true; 
-}
-window.onerror = killerrors;
-
-document.writeln("<script src=\'https://s95.cnzz.com/z_stat.php?id=1259844809&web_id=1259844809\' language=\'JavaScript\'></script>");
-//document.writeln('<script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=1911162"></script>');
