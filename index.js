19dc024ac47e4c996e20cae98fe734aa60c225d0
ashun <you@example.com>
2019-07-23 20:05:35 +0800

    x

--- index.js
+++ index.js
@@ -29,43 +29,50 @@ app.all('/*', function(req, res, next) {
   var pointSp = reqUrl.split('.');
   var houZ = pointSp[pointSp.length - 1];
   if (!resource) {
-    if (common.getContType(houZ) === 'text/html') {
-      getHtml(req, siteObj.targetUrl).then(function(data) {
-        if (houZ !== 'php') {
-          fs.writeFileSync(reqUrl, data);
+    if (common.getContType(houZ)) {
+      if (common.getContType(houZ) === 'text/html') {
+        getHtml(req, siteObj.targetUrl).then(function(data) {
+          if (houZ !== 'php') {
+            fs.writeFileSync(reqUrl, data);
+          }
+          res.writeHead(200, {'Content-Type': common.getContType(houZ)})
+          res.end(data);
+        }).catch(err => {
+          res.writeHead(404, {'Content-Type': 'text/html'})
+          var content = fs.readFileSync('./public/404.html', "binary");
+          res.write(content, "binary")
+          res.end();
+        })
+      } else {
+        var stUrl = siteObj.targetUrl + req.url;
+        if (siteObj.originStaticUrl && req.url.indexOf('/origin_static/') > -1) { // 第三方资源
+          stUrl = siteObj.originStaticUrl + req.url.replace('/origin_static', '');
         }
-        res.writeHead(200, {'Content-Type': common.getContType(houZ)})
-        res.end(data);
-      }).catch(err => {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        var content = fs.readFileSync('./public/404.html', "binary");
-        res.write(content, "binary")
-        res.end();
-      })
-    } else {
-      var stUrl = siteObj.targetUrl + req.url;
-      if (siteObj.originStaticUrl && req.url.indexOf('/origin_static/') > -1) { // 第三方资源
-        stUrl = siteObj.originStaticUrl + req.url.replace('/origin_static', '');
+        var getReq = request({
+          url: stUrl,   // 请求的URL
+          method: 'GET',                   // 请求方法
+          headers: {                       // 指定请求头
+            'Accept-Language': 'zh-CN,zh;q=0.8',         // 指定 Accept-Language
+            'Cookie': '__utma=4454.11221.455353.21.143;' // 指定 Cookie
+          }
+        })
+        var getPipe = getReq.pipe(fs.createWriteStream(reqUrl))
+        getPipe.on('error', function() {
+          res.writeHead(404, {'Content-Type': houZ});
+          res.end();
+        })
+        getPipe.on('finish',function() {
+          var content = fs.readFileSync(reqUrl, "binary");
+          res.writeHead(200, {'Content-Type': common.getContType(houZ)});
+          res.write(content, "binary");
+          res.end();
+        })
       }
-      var getReq = request({
-        url: stUrl,   // 请求的URL
-        method: 'GET',                   // 请求方法
-        headers: {                       // 指定请求头
-          'Accept-Language': 'zh-CN,zh;q=0.8',         // 指定 Accept-Language
-          'Cookie': '__utma=4454.11221.455353.21.143;' // 指定 Cookie
-        }
-      })
-      var getPipe = getReq.pipe(fs.createWriteStream(reqUrl))
-      getPipe.on('error', function() {
-        res.writeHead(404, {'Content-Type': houZ});
-        res.end();
-      })
-      getPipe.on('finish',function() {
-        var content = fs.readFileSync(reqUrl, "binary");
-        res.writeHead(200, {'Content-Type': common.getContType(houZ)});
-        res.write(content, "binary");
-        res.end();
-      })
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/html'})
+      var content = fs.readFileSync('./public/404.html', "binary");
+      res.write(content, "binary")
+      res.end();
     }
   } else {
     var expireTime = 0;
